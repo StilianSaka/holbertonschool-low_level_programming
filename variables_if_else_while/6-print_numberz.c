2e18f67d71025b2de8c5eb1f621e6d9c978aad20
StilianSaka <stilisaka@gmail.com>
2024-06-18 12:08:02 +0000

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 0; c < 10; c++)
+{
+putchar("%d", c);
+}
+putchar('\n');
+return (0);
+}
