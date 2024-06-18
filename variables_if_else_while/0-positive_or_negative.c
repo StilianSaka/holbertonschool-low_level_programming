feb7240df094f4d81a26827cd9e934bff4dd0c3f
StilianSaka <stilisaka@gmail.com>
2024-06-18 10:52:17 +0000

    first

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -20,10 +20,12 @@ if (n > 0)
 {
 printf("%d is positive\n", n);        
 }
-if (n == 0){
+if (n == 0)
+{
 printf("%d is zero\n", n);
 }
-if (n < 0){
+if (n < 0)
+{
 printf("%d is negative\n", n);
 }
         
