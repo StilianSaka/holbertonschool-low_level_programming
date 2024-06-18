a6a7cff225318d44fe42720ea7e4d2119acca098
StilianSaka <stilisaka@gmail.com>
2024-06-18 12:53:03 +0000

    commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,15 +1,9 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+
 int main(void)
 {
+    for (int i = 0; i <= 9; i++)
+    {
         for (int j = i + 1; j <= 9; j++)
         {
             putchar(i + '0');
