bb05554c8821f793deafa311232284db3085c958
StilianSaka <stilisaka@gmail.com>
2024-06-20 08:26:39 +0000

    print

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  *
  *
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 char c;
 int i;
 for (i = 0; i <= 9 ; i++)
 {
 	for (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
+		_putchar(c);
 	_putchar('\n');
 }
 }
