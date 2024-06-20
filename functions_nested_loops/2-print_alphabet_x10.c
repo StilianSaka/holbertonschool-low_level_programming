1b1ed67c8abd856956bef47ac72af1afec5ce71e
StilianSaka <stilisaka@gmail.com>
2024-06-20 08:34:15 +0000

    print

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - print the alphabet in lowercase 10 times
+ * print_alphabet_x10 - print the alphabet in lowercase 10 times
  *
  *
  */
 void print_alphabet_x10(void)
+
 {
 char c;
 int i;
 for (i = 0; i <= 9 ; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
+	_putchar(c);
 _putchar('\n');
 }
 }
