0b5b00bfc73fa90805e0e0f4f4f19a670deb10cc
StilianSaka <stilisaka@gmail.com>
2024-06-20 10:56:32 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _abs - computes the absolute value of an integrer
+ * @number: number to check
+ *
+ * return number if number is positive
+ * return -number else number is negative
+ */
+
+int _abs(int number)
+{
+	if (number > 0)
+		return (number);
+	else
+		return (-number);
+}
