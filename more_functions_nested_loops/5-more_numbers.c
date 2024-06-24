00bc821906962139c880a5e46a83e2d72af5bb97
StilianSaka <stilisaka@gmail.com>
2024-06-24 10:45:24 +0000

    more

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  more_numbers_x10 - print the alphabet in lowercase 10 times
+ *
+ *
+ */
+void more_numbers(void)
+
+{
+int i;
+for (i = 0; i <= 14 ; i++)
+{
+	_putchar(i);
+_putchar('\n');
+}
+}
