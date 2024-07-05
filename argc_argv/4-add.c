e1ed330533f67db5c27dcff4754d3918c1c18200
StilianSaka <stilisaka@gmail.com>
2024-07-05 09:04:57 +0000

    changes

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,12 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, result;
+	int i, j, result;
 
 	result = 0;
 	for (i = 0; i < argc; i++)
 	{
 		result += atoi(argv[i]);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
 
 	printf("%d\n", result);
