e093c467992c1d84bf7a9e6ea9ca9515dca31602
StilianSaka <stilisaka@gmail.com>
2024-07-05 08:10:03 +0000

    whats

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
-* @argc: argument count
-* @argv: argument array
-*
-*Return: Always 0.
-*/
-
-int main(int argc __attribute__((unused)), char *argv[])
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ */
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
