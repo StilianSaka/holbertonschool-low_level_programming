d1bcb219c307f5e8e6cfe39592890893e19c2d50
StilianSaka <stilisaka@gmail.com>
2024-07-05 08:18:37 +0000

    whats

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - entry point
  * @argc: argument count
  * @argv: argument array
- * return: Always 0.
+ * Return: Always 0.
  */
 int main(int argc __attribute__((__unused__)), char *argv[])
 {
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,7 +3,7 @@
  * main - entry point
  * @argc: argument count
  * @argv: argument array
-  * return: Always 0.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[] __attribute__((__unused__)))
 {
