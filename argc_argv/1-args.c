6065502f329e0ad8fb01d00e979825392bb86c9c
StilianSaka <stilisaka@gmail.com>
2024-07-05 08:14:44 +0000

    whats

---
+++ argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ */
+int main(int argc, char *argv[] __attribute__((__unused__)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
