e8edf95bad65ef63ae1949fb9ba0436637117170
StilianSaka <stilisaka@gmail.com>
2024-07-11 10:57:07 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - define a dog type
- * @a: Dog's name
- * @b: Dog's age
- * @c: Dog's owner
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
  *
  * Description: a custom defined structure of a dog
  */
