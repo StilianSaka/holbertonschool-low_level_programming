271bcf0669f7b4e7b51cf5149d4ca8876c592094
StilianSaka <stilisaka@gmail.com>
2024-07-18 12:18:05 +0000

    sta

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,13 @@
 #include <stdarg.h>
 
+/**
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list numbers;
