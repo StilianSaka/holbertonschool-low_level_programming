fbbc40a1eee30c395c3a42b8bfee52f43c85a1e1
StilianSaka <stilisaka@gmail.com>
2024-07-19 09:15:45 +0000

    prall

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
