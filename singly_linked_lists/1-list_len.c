51a0fdfc6d1770ada62d803d066ea2debeb9eb8f
StilianSaka <stilisaka@gmail.com>
2024-07-29 11:48:01 +0000

    list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - Prints all elements of a list_t list.
+* @h: Pointer to the head of the list.
+* Return: The number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
