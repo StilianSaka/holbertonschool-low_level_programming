8c8bb86b95b9408ccc44c1643ab100c9a5f202dd
StilianSaka <stilisaka@gmail.com>
2024-08-04 21:00:30 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,6 +14,12 @@ void handle_error(int exit_code, char *message, char *file_name)
 	exit(exit_code);
 }
 
+void handle_fd_error(int exit_code, char *message, int fd_value)
+{
+	dprintf(STDERR_FILENO, message, fd_value);
+	exit(exit_code);
+}
+
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, bytes_read, bytes_written;
@@ -21,24 +27,30 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		handle_error(97, "Usage: cp %s\n", NULL);
+	
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		handle_error(98, "Error: Can't read from file %s\n", argv[1]);
+	
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 		handle_error(99, "Error: Can't write to %s\n", argv[2]);
+	
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 			handle_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
+	
 	if (bytes_read == -1)
 		handle_error(98, "Error: Can't read from file %s\n", argv[1]);
+	
 	if (close(fd_from) == -1)
-		handle_error(100, "Error: Can't close fd\n", NULL);
+		handle_fd_error(100, "Error: Can't close fd %d\n", fd_from);
+	
 	if (close(fd_to) == -1)
-		handle_error(100, "Error: Can't close fd\n", NULL);
+		handle_fd_error(100, "Error: Can't close fd %d\n", fd_to);
 
 	return (0);
 }
