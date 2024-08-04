7ee68862fd42b57aa326655c806946f479f339b9
StilianSaka <stilisaka@gmail.com>
2024-08-04 20:54:59 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,12 +5,10 @@
 
 #define BUFFER_SIZE 1024
 
-void handle_error(int exit_code, char *message, char *file_name, int fd_value)
+void handle_error(int exit_code, char *message, char *file_name)
 {
 	if (file_name)
 		dprintf(STDERR_FILENO, message, file_name);
-	else if (fd_value >= 0)
-		dprintf(STDERR_FILENO, message, fd_value);
 	else
 		dprintf(STDERR_FILENO, "%s\n", message);
 	exit(exit_code);
@@ -22,24 +20,25 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		handle_error(97, "Usage: cp %s\n", NULL, -1);
+		handle_error(97, "Usage: cp %s\n", NULL);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		handle_error(98, "Error: Can't read from file %s\n", argv[1], -1);
+		handle_error(98, "Error: Can't read from file %s\n", argv[1]);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		handle_error(99, "Error: Can't write to %s\n", argv[2], -1);
+		handle_error(99, "Error: Can't write to %s\n", argv[2]);
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
-			handle_error(99, "Error: Can't write to %s\n", argv[2], -1);
+			handle_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
 	if (bytes_read == -1)
-		handle_error(98, "Error: Can't read from file %s\n", argv[1], -1);
+		handle_error(98, "Error: Can't read from file %s\n", argv[1]);
 	if (close(fd_from) == -1)
-		handle_error(100, "Error: Can't close fd\n", NULL, fd_from);
+		handle_error(100, "Error: Can't close fd\n", NULL);
 	if (close(fd_to) == -1)
-		handle_error(100, "Error: Can't close fd\n", NULL, fd_to);
+		handle_error(100, "Error: Can't close fd\n", NULL);
+
 	return (0);
 }
